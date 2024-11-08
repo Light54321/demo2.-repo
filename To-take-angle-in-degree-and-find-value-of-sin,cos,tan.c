07245feaf29029f986dca39764e21740568a5b57
Aditya singh <rajsinght932@gmail.com>
2024-11-08 21:23:16 +0530

    5

--- To-take-angle-in-degree-and-find-value-of-sin,cos,tan.c
+++ To-take-angle-in-degree-and-find-value-of-sin,cos,tan.c
@@ -3,6 +3,8 @@ void main()
 {
 double radius,pi=3.14,area,length,areaofsquare;
 printf(" please enter the radius a of circle");
+printf(" please enter the radius a of");
+printf(" please enter the radius a ");
 printf(" please enter the radius ");
 printf(" please enter the ");
 printf(" please enter ");
